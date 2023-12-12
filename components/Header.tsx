31ed942a43a0200ab7788464504e8b48d4af78dc
30jannik06 <80638124+30jannik06@users.noreply.github.com>
2023-12-12 14:04:36 +0100

    🔅Update Design

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,25 +1,26 @@
 "use client";
+import React from "react";
 import { useLanguage } from "./LanguageProvider";
 import LanguageToggler from "./LanguageToggler";
 import { Logo } from "./Logo";
-import { Label } from "./ui/label";
 
 export const Header = () => {
     const { language } = useLanguage();
 
     return (
-        <header className="bg-[#213b8d] flex items-center p-4">
-            <Logo />
-            <Label className="text-white text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl font-bold ml-4 sm:ml-6">
-                {language === "de" ? "Web Radio Spieler" : "Web Radio Player"}
-            </Label>
-            <div className="ml-auto flex items-center">
-                {/* <Label className="text-white mr-2 hidden sm:block">
+        <header className="bg-blue-900 text-white py-4">
+            <div className="container mx-auto flex items-center justify-between">
+                <Logo />
+                <div className="flex items-center">
+                    <LanguageToggler />
+                </div>
+            </div>
+            <div className="container mx-auto mt-4">
+                <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold">
                     {language === "de"
-                        ? "Sprache auswählen:"
-                        : "Select Language:"}
-                </Label> */}
-                <LanguageToggler />
+                        ? "Web Radio Spieler"
+                        : "Web Radio Player"}
+                </h1>
             </div>
         </header>
     );
