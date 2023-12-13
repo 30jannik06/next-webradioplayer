25cab4920f08c32fd4254d9ca3cc8365ef0924b7
30jannik06 <80638124+30jannik06@users.noreply.github.com>
2023-12-13 09:09:41 +0100

    💯Add/Update VolumeController

---
+++ components/VolumeControl.tsx
@@ -0,0 +1,38 @@
+import React, { ChangeEvent } from "react";
+import { Label } from "./ui/label";
+import { Input } from "./ui/input";
+import { IVolumeControlProps } from "@/interfaces/IVolumeControlProps";
+
+const VolumeControl: React.FC<IVolumeControlProps> = ({
+    volume,
+    onVolumeChange,
+    language,
+}) => {
+    const handleVolumeChange = (event: ChangeEvent<HTMLInputElement>) => {
+        onVolumeChange(parseFloat(event.target.value));
+    };
+
+    return (
+        <div className="flex flex-col items-center mb-4">
+            <Label className="text-white mt-4 mb-3">
+                {language === "de" ? "Lautstärke:" : "Volume:"}
+            </Label>
+            <div className="relative w-full">
+                <Input
+                    type="range"
+                    min={0}
+                    max={1}
+                    step={0.01}
+                    value={volume}
+                    onChange={handleVolumeChange}
+                    className="w-full h-6 appearance-none bg-gray-700 rounded-full"
+                />
+                <div className="absolute left-1/2 top-0 transform -translate-x-1/2 mt-6 text-white">
+                    {Math.round(volume * 100)}%
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default VolumeControl;
