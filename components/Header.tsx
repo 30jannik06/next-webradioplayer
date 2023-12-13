2e3bf3f6008e7d66d4539b5e916d9ac0265a9ccc
30jannik06 <80638124+30jannik06@users.noreply.github.com>
2023-12-13 09:11:43 +0100

    👾Update randoms

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,9 @@
 "use client";
 import React from "react";
-import { useLanguage } from "./LanguageProvider";
 import LanguageToggler from "./LanguageToggler";
 import { Logo } from "./Logo";
 
 export const Header = () => {
-    const { language } = useLanguage();
-
     return (
         <header className="bg-blue-900 text-white py-4">
             <div className="container mx-auto flex items-center justify-between">
@@ -17,9 +14,7 @@ export const Header = () => {
             </div>
             <div className="container mx-auto mt-4">
                 <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold">
-                    {language === "de"
-                        ? "Web Radio Spieler"
-                        : "Web Radio Player"}
+                    Web Radio Player
                 </h1>
             </div>
         </header>
