fe04568c1384ea720e3c7c25c084e01e05a254f3
30jannik06 <80638124+30jannik06@users.noreply.github.com>
2024-01-16 15:22:21 +0100

    🎉Add Mute Button

--- RadioPlayer/components/VolumeControl.tsx
+++ RadioPlayer/components/VolumeControl.tsx
@@ -1,7 +1,12 @@
-import React, { ChangeEvent } from "react";
+import React, { ChangeEvent, useState } from "react";
 import { Label } from "./ui/label";
 import { Input } from "./ui/input";
 import { IVolumeControlProps } from "@/interfaces/IVolumeControlProps";
+import { SpeakerLoudIcon, SpeakerOffIcon } from "@radix-ui/react-icons";
+import { Button } from "./ui/button";
+
+const SPEAKER_LOUD_ICON = <SpeakerLoudIcon className="mr-2" />;
+const SPEAKER_OFF_ICON = <SpeakerOffIcon className="mr-2" />;
 
 export const VolumeControl: React.FC<IVolumeControlProps> = ({
     volume,
@@ -9,9 +14,22 @@ export const VolumeControl: React.FC<IVolumeControlProps> = ({
     language,
 }) => {
     const stepPercentage = 5;
+    const [isMuted, setIsMuted] = useState(false);
+    const previousVolume = 0.5;
 
     const handleVolumeChange = (event: ChangeEvent<HTMLInputElement>) => {
-        onVolumeChange(parseFloat(event.target.value));
+        if (!isMuted) {
+            onVolumeChange(parseFloat(event.target.value));
+        }
+    };
+
+    const handleMuteToggle = () => {
+        if (isMuted) {
+            onVolumeChange(previousVolume);
+        } else {
+            onVolumeChange(0);
+        }
+        setIsMuted(!isMuted);
     };
 
     const calculateStep = () => {
@@ -23,17 +41,25 @@ export const VolumeControl: React.FC<IVolumeControlProps> = ({
             <Label className="text-white mt-4 mb-3 text-lg">
                 {language === "de" ? "Lautstärke:" : "Volume:"}
             </Label>
-            <div className="relative w-full">
-                <Input
-                    type="range"
-                    min={0}
-                    max={1}
-                    step={calculateStep()}
-                    value={volume}
-                    onChange={handleVolumeChange}
-                    className="w-full h-6 appearance-none bg-gradient-to-r from-purple-500 to-pink-500 rounded-full"
-                />
-                <div className="absolute left-1/2 top-0 transform -translate-x-1/2 mt-6 text-white">
+            <div className="relative w-full flex items-center justify-center">
+                <Button
+                    onClick={handleMuteToggle}
+                    className="mr-4 text-white cursor-pointer focus:outline-none"
+                >
+                    {isMuted ? SPEAKER_OFF_ICON : SPEAKER_LOUD_ICON}
+                </Button>
+                <div className="w-full">
+                    <Input
+                        type="range"
+                        min={0}
+                        max={1}
+                        step={calculateStep()}
+                        value={volume}
+                        onChange={handleVolumeChange}
+                        className="w-full h-6 appearance-none bg-gradient-to-r from-purple-500 to-pink-500 rounded-full"
+                    />
+                </div>
+                <div className="ml-4 text-white">
                     {Math.round(volume * 100)}%
                 </div>
             </div>
