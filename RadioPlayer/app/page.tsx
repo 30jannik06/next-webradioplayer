f9c57b90c3a9acfd5109dde83bb236406ea93321
Jannik Sommer <mcyolo17@gmail.com>
2024-04-17 11:16:58 +0200

    Add Loading UI

--- RadioPlayer/app/page.tsx
+++ RadioPlayer/app/page.tsx
@@ -1,19 +1,22 @@
 "use client";
 
-import React from "react";
+import React, { Suspense } from "react";
 import { AudioPlayer } from "@/components/AudioPlayer";
 import { LanguageProvider } from "@/components/LanguageProvider";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
+import LoadingScreen from "./loading";
 
 const Home: React.FC = () => {
     return (
         <LanguageProvider>
-            <Header />
-            <div className="container mx-auto p-4">
-                <AudioPlayer/>
-            </div>
-            <Footer />
+            <Suspense fallback={<LoadingScreen />}>
+                <Header />
+                <div className="container mx-auto p-4">
+                    <AudioPlayer />
+                </div>
+                <Footer />
+            </Suspense>
         </LanguageProvider>
     );
 };
